91d882088dcfc303ef503f1af8678838ec28e2f7
Karan <karansharma1295@gmail.com>
2016-04-09 16:05:36 +0530

    nutrition

--- clouds.py
+++ clouds.py
@@ -8,3 +8,9 @@ if status['status'] != cloudsight.STATUS_NOT_COMPLETED:
     # Done!
     pass
 status = api.wait(response['token'], timeout=30)
+
+
+from nutritionix import Nutritionix
+nix = Nutritionix(app_id="76986486", api_key="28882f3d105c4c9e3222a05eeafd049a")
+
+result = nix.search('pizza').json()
